c308acd85be19e746d9737937efa52141cd0a792
Mulu Beshada <mulubeshada2@gmail.com>
2026-03-06 08:38:48 -0300

    connect frontend with backend

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    fetch('/api/message')
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error('Error fetching message:', error))
+  })
+}
+
+export default App
