016550a9017fd0f948b46e37bd4ca48ebd04a63f
Mulu Beshada <mulubeshada2@gmail.com>
2026-03-06 10:05:50 -0300

    setup shadcn ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,17 +1,22 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch('/api/message')
+    fetch("/api/hello")
       .then((response) => response.json())
       .then((data) => setMessage(data.message))
-      .catch((error) => console.error('Error fetching message:', error))
-  })
+      .catch((error) => console.error("Error fetching message:", error));
+  }, []);
+
+  return (
+    <div className="p-4">
+      <p className="font-bold text-3xl text-blue-500">{message}</p>
+      <Button>Click Me</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
